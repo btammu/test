3d2b8955fda559925789a4c1c2ce9cf9fea0fe2b
btammu <133737808+btammu@users.noreply.github.com>
2023-05-16 19:45:04 +0530

    Update stackoverflow.sh

--- stackoverflow.sh
+++ stackoverflow.sh
@@ -1,11 +1,3 @@
-
-json=$(curl -v -u $user1:$pass1 -X POST \'https://budda-tammu.atlassian.net/rest/api/2/myself\')
-echo $json
-token=$(echo $json | sed "s/{.*\\"Authorization\\":\\"\\([^\\"]*\\).*}/\\1/g")
-echo $token
-
-
-
 json=$(curl --location -v -u $user1:$pass1 -X POST 'https://budda-tammu.atlassian.net/rest/api/2/myself' --header 'Content-Type: application/x-www-form-urlencoded')
 echo $json
 token=$(echo $json | sed "s/{.*\\"Authorization\\":\\"\\([^\\"]*\\).*}/\\1/g")
