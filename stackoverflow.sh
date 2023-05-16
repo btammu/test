d1d7959d1ef6bf9f8774120ec27a3b9106ab8fb5
btammu <133737808+btammu@users.noreply.github.com>
2023-05-16 20:40:47 +0530

    Update stackoverflow.sh

--- stackoverflow.sh
+++ stackoverflow.sh
@@ -4,5 +4,5 @@ echo "Curl executed succeeded!!!"
 echo "Data of curl is $json"
 echo "Curl executed succeeded!!!"
 echo "Data of curl is $newtoken"
-curl -X POST 'https://budda-tammu.atlassian.net/rest/api/2/issue/' --header 'Accept:application/json' --header 'Authorization: Basic $newtoken' -d '{"fields": {"project":{"key":"BTAM"}, "summary": "REST EXAMPLE API", "description": "Creating an issue via REST API", "issuetype": {"name": "Task"}}}'
+curl -X POST 'https://budda-tammu.atlassian.net/rest/api/2/issue/' --header 'Accept:application/json' --header 'Authorization: Basic $newtoken' --data '{"fields": {"project":{"key":"BTAM"}, "summary": "REST EXAMPLE API", "description": "Creating an issue via REST API", "issuetype": {"name": "Task"}}}'
 echo "Sonar Quality gate succeeded!!!"
