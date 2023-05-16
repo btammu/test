e6859f5395676d06c1fc74cdb755a58453686acd
btammu <133737808+btammu@users.noreply.github.com>
2023-05-16 10:57:17 +0530

    Update stackoverflow.sh

--- stackoverflow.sh
+++ stackoverflow.sh
@@ -1,3 +1,3 @@
-token ="curl -v -u btammu:Budda@123TCS -X POST https://api.github.com/authorizations -s"
+token =$(curl -v -u btammu:Budda@123TCS -X POST https://api.github.com/authorizations -s)
 
 curl -H "Authorization: $token" https://api.github.com/user
