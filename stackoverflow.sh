0fff7514d82377ea150204ab16da1acea93cf97c
btammu <133737808+btammu@users.noreply.github.com>
2023-05-16 13:21:16 +0530

    Update stackoverflow.sh

--- stackoverflow.sh
+++ stackoverflow.sh
@@ -1,5 +1,6 @@
-token =$(curl -v -u btammu:Budda@123TCS -X POST https://github.com/login/oauth/access_token -s)
 
-echo $token
+AUTHORIZATION=$(curl --location -u btammu:Budda@123TCS --request POST 'https://api.github.com/authorizations' --header 'Content-Type: application/x-www-form-urlencoded')
 
+AUTHORIZATION = `jq '.access_token' <<< "$AUTHORIZATION"`
 
+AUTHORIZATION=`echo "$AUTHORIZATION" | tr -d '"'`.
