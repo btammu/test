4413b093430cbe71b861c7e8bf33609df3c83242
btammu <133737808+btammu@users.noreply.github.com>
2023-05-16 19:51:11 +0530

    Update stackoverflow.sh

--- stackoverflow.sh
+++ stackoverflow.sh
@@ -1,3 +1,2 @@
 json=$(curl -v -u $user1:$pass1 -X POST 'https://budda-tammu.atlassian.net/rest/api/2/myself' --header 'Content-Type:application/json' --header 'Accept:application/json')
-echo $json
-
+echo "$json"
