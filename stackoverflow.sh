46a78668e37ebd6ab32f6a132efa34467af7f0b1
btammu <133737808+btammu@users.noreply.github.com>
2023-05-16 19:50:17 +0530

    Update stackoverflow.sh
    nsahb

--- stackoverflow.sh
+++ stackoverflow.sh
@@ -1,4 +1,3 @@
-json=$(curl --location -v -u $user1:$pass1 -X POST 'https://budda-tammu.atlassian.net/rest/api/2/myself' --header 'Content-Type: application/x-www-form-urlencoded')
+json=$(curl -v -u $user1:$pass1 -X POST 'https://budda-tammu.atlassian.net/rest/api/2/myself' --header 'Content-Type:application/json' --header 'Accept:application/json')
 echo $json
-token=$(echo $json | sed "s/{.*\\"Authorization\\":\\"\\([^\\"]*\\).*}/\\1/g")
-echo $token
+
