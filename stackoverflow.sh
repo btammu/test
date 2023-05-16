77510dbafb8fe9e4f69b9714fd492ae389f2ade6
btammu <133737808+btammu@users.noreply.github.com>
2023-05-16 20:02:51 +0530

    Update stackoverflow.sh

--- stackoverflow.sh
+++ stackoverflow.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 json=$(curl -v -u $user1:$pass1 -X POST 'https://budda-tammu.atlassian.net/rest/api/2/myself' --header 'Authorization: Basic $newtoken' --header 'Accept:application/json')
-echo "Sonar Quality gate succeeded!!!"
-echo "$json"
+echo "Curl executed succeeded!!!"
+echo "Data of curl is $json"
+echo "Curl executed succeeded!!!"
+echo "Data of curl is $newtoken"
 echo "Sonar Quality gate succeeded!!!"
