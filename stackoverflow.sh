afa8a9949111b781c2308bf7fe2c51362bf5407f
btammu <133737808+btammu@users.noreply.github.com>
2023-05-17 14:59:41 +0530

    Update stackoverflow.sh

--- stackoverflow.sh
+++ stackoverflow.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-token=$(curl -kv -u $user1:$pass1 -X POST 'https://budda-tammu.atlassian.net/rest/api/2/myself' --header 'Accept:application/json' --header 'Content-Type: application/json' -S -f)
+token=$(curl -u $user1:$pass1 -X POST 'https://budda-tammu.atlassian.net/rest/api/2/myself' --header 'Accept:application/json' --header 'Content-Type: application/json' -S -f)
 echo "Curl executed succeeded!!!"
 echo "Data of curl is $token"
 echo "Curl executed succeeded!!!"
 echo "Data of curl is $newtoken"
-curl -f -X POST 'https://budda-tammu.atlassian.net/rest/api/2/issue/' --header 'Content-Type: application/json' --header 'Authorization: Basic $newtoken' --data '{"fields": {"project":{"key": "BTAM"}, "summary": "REST EXAMPLE API", "description": "Creating an issue via REST API", "issuetype": {"name": "Task"}}}'
+curl -f -S -X POST 'https://budda-tammu.atlassian.net/rest/api/2/issue/' --header 'Content-Type: application/json' --header 'Authorization: Basic $newtoken' --data '{"fields": {"project":{"key": "BTAM"}, "summary": "REST EXAMPLE API", "description": "Creating an issue via REST API", "issuetype": {"name": "Task"}}}'
 echo "Jira created successfully"
