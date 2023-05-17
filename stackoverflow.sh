8dcd1068cb68f24e28e946a513ec7fd662c75f7a
btammu <133737808+btammu@users.noreply.github.com>
2023-05-17 14:07:14 +0530

    Update stackoverflow.sh

--- stackoverflow.sh
+++ stackoverflow.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-token=$(curl -v -u $user1:$pass1 -X POST 'https://budda-tammu.atlassian.net/rest/api/2/myself' --header 'Authorization: Basic $newtoken' --header 'Accept:application/json' --header 'Content-Type: application/json')
+token=$(curl -v -i -u $user1:$pass1 -X POST 'https://budda-tammu.atlassian.net/rest/api/2/myself' --header 'Authorization: Basic $newtoken' --header 'Accept:application/json' --header 'Content-Type: application/json')
 echo "Curl executed succeeded!!!"
 echo "Data of curl is $token"
 echo "Curl executed succeeded!!!"
