8323b8e20125bcbcd7c18739fecb451d932bc32f
btammu <133737808+btammu@users.noreply.github.com>
2023-05-17 09:59:55 +0530

    Update stackoverflow.sh
    Updated shell

--- stackoverflow.sh
+++ stackoverflow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-json=$(curl -v -u $user1:$pass1 -X POST 'https://budda-tammu.atlassian.net/rest/api/2/myself' --header 'Authorization: Basic $newtoken' --header 'Accept:application/json')
+token=$(curl -v -u $user1:$pass1 -X POST 'https://budda-tammu.atlassian.net/rest/api/2/myself' --header 'Authorization: Basic $newtoken' --header 'Accept:application/json' --header 'Content-Type: application/json')
 echo "Curl executed succeeded!!!"
-echo "Data of curl is $json"
+echo "Data of curl is $token"
 echo "Curl executed succeeded!!!"
 echo "Data of curl is $newtoken"
 curl -X POST 'https://budda-tammu.atlassian.net/rest/api/2/issue/' --header 'Content-Type: application/json' --header 'Authorization: Basic $newtoken' --data '{"fields": {"project":{"key": "BTAM"}, "summary": "REST EXAMPLE API", "description": "Creating an issue via REST API", "issuetype": {"name": "Task"}}}'
